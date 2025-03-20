6401baa0c41d034bf3c742806b5ff4b0248325bb
Jungwoo Yang <jwyang0213@gmail.com>
2025-03-21 02:24:58 +0900

    add headers

--- fetch_latest.py
+++ fetch_latest.py
@@ -5,14 +5,14 @@ import datetime
 
 def main():
     # Fetch the latest news
-    response = requests.get("https://gsds.snu.ac.kr/news/news-seminar/")
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
+    }
+    response = requests.get("https://gsds.snu.ac.kr/news/news-seminar/", headers=headers)
     soup = BeautifulSoup(response.text, "html.parser")
     element = soup.find("div", class_="loop").find("h4", class_="title").a
     url = element["href"]
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
-    }
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, "html.parser")
     element = soup.main.find("div", class_="container")
